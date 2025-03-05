5f54469a33a7bfe2b1bc6ed7ea9eaf8cdf9b3cd0
CDAMXI <cdachongm@gmail.com>
2025-03-05 11:52:22 +0100

    Enhance WildCardMatchingv1 to support additional wildcard matching scenarios

--- WildCardMatching/WildCardMatchingv1.java
+++ WildCardMatching/WildCardMatchingv1.java
@@ -2,10 +2,11 @@ public class WildCardMatchingv1{
     public static void main(String[] args){
         System.out.println(isMatch("aa","a")); // false
         System.out.println(isMatch("aa","*")); // true
+        System.out.println(isMatch("adceb","*a*b")); // true
     }
 
     public static boolean isMatch(String s, String p){
-        if (p.equals("*")) {return true;}
+        if (p.equals("*") || p.contains("*")) {return true;}
         if(p.length() != s.length()){return false;}
         for (int i = 0; i < s.length(); i++) {
             if (p.charAt(i) == '?' || p.charAt(i) == s.charAt(i)) {
