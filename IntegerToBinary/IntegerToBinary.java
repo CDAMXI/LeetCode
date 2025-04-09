72647a308713805e4997a191d87d82b9ac01581b
CDAMXI <cdachongm@gmail.com>
2025-04-09 11:11:12 +0200

    Reimplement Integer to Binary conversion in Python with user input handling

---
+++ IntegerToBinary/IntegerToBinary.java
@@ -0,0 +1,7 @@
+public class IntegerToBinary{
+    public static void main(String[] args) {
+        int number = 10; // Example integer
+        String binaryString = Integer.toBinaryString(number);
+        System.out.println("The binary representation of " + number + " is: " + binaryString);
+    }
+}
