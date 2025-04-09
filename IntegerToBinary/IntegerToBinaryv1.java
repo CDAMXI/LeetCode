7a9d8df6b16419193d2be6d07a2d402b2a50cc56
CDAMXI <cdachongm@gmail.com>
2025-04-09 11:21:01 +0200

    Refactor Integer to Binary conversion program for clarity and input handling

--- IntegerToBinary/IntegerToBinaryv1.java
+++ IntegerToBinary/IntegerToBinaryv1.java
@@ -1,14 +1,18 @@
 import java.util.*;
 
 public class IntegerToBinaryv1{
-    public static void main(String[] args) {
-        int number = 0;
+    public static void main(String[] args){
         Scanner kbd = new Scanner(System.in);
+        int n = 0;
+
         do{
-            System.out.print("Enter a non-negative integer (or a negative number to exit): " );
-            String binaryString = Integer.toBinaryString(number);
-            System.out.println("The binary representation of " + number + " is: " + binaryString);
-        } while(number < 0);
-        kbd.close();
+            System.out.print("Enter a positive integer (negative number to quit): ");
+            n = kbd.nextInt();
+            if(n > 0){
+                System.out.println("The binary representation of " + n + " is: " + Integer.toBinaryString(n));
+            } else if(n < 0){
+                System.out.println("Please enter a positive integer.");
+            }
+        } while(!(n < 0));
     }
 }
