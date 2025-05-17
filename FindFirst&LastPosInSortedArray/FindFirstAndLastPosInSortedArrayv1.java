ebd7326b750c3c56a234ba7ad5abc46dcb679262
CDAMXI <cdachongm@gmail.com>
2025-05-17 12:19:49 +0200

    Add implementations for FindFirstAndLastPosInSortedArray in two versions

---
+++ FindFirst&LastPosInSortedArray/FindFirstAndLastPosInSortedArrayv1.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class FindFirstAndLastPosInSortedArrayv1{
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(searchRange(new int[]{5, 7, 7, 8, 8, 10}, 8)));
+    }
+
+    public static int[] searchRange(int[] nums, int target) {
+        if(nums == null || nums.length == 0) {return new int[]{-1, -1};}
+        if(nums.length == 1 && nums[0] == target){return new int[]{0,0};}
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target && result[0] == 0) {result[0] = i;}
+            if (nums[i] == target) {result[1] = i;}
+            if (nums[i] > target) {break;}
+        }
+        if (result[0] == 0 && result[1] == 0) {return new int[]{-1, -1};}
+        return result;
+    }
+}
