2ab3d52deb3b7adc9e5640324bae9a2113aa4868
CDAMXI <cdachongm@gmail.com>
2025-05-17 12:30:15 +0200

    Refactor FindFirstAndLastPositionv2: make methods static and add main for testing

--- FindFirst&LastPosInSortedArray/FindFirstAndLastPositionv2.java
+++ FindFirst&LastPosInSortedArray/FindFirstAndLastPositionv2.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
+
 public class FindFirstAndLastPositionv2 {
-    public int[] searchRange(int[] nums, int target) {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(searchRange(new int[]{5, 7, 7, 8, 8, 10}, 8)));
+    }
+
+    public static int[] searchRange(int[] nums, int target) {
         int first = findBound(nums, target, true);
         if (first == -1) {
             return new int[]{-1, -1};
@@ -9,7 +15,7 @@ public class FindFirstAndLastPositionv2 {
     }
 
     // Helper to find either first or last occurrence
-    private int findBound(int[] nums, int target, boolean findFirst) {
+    private static int findBound(int[] nums, int target, boolean findFirst) {
         int low = 0, high = nums.length - 1, result = -1;
 
         while (low <= high) {
