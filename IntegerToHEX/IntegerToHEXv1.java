e6e706b124bb4f7cfd9a8fa9a4de8fc9fb032a3a
CDAMXI <cdachongm@gmail.com>
2025-05-21 08:16:58 +0200

    Fix casing in output messages for HEX conversion

--- IntegerToHEX/IntegerToHEXv1.java
+++ IntegerToHEX/IntegerToHEXv1.java
@@ -1,7 +1,7 @@
 public class IntegerToHEXv1{
     public static void main(String[] args) {
-        System.out.println("26 in hex is : " + toHex(26));
-        System.out.println("255 in hex is : " + toHex(255));
+        System.out.println("26 in HEX is : " + toHex(26));
+        System.out.println("255 in HEX is : " + toHex(255));
     }
 
     public static String toHex(int num){
