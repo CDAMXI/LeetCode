a23b1d1069adf24771489e0994bc36df1d13349b
CDAMXI <cdachongm@gmail.com>
2025-07-30 12:50:28 +0200

    Add additional test cases to main method for removeDuplicates functionality

--- RemoveDuplicatesFromSortedArrayII/RemoveDuplicatesFromSortedArrayIIv2.java
+++ RemoveDuplicatesFromSortedArrayII/RemoveDuplicatesFromSortedArrayIIv2.java
@@ -4,6 +4,12 @@ public class RemoveDuplicatesFromSortedArrayIIv2 {
         System.out.println("For \"[1,1,1,2,2,3]\", new length = " + removeDuplicates(new int[]{1, 1, 1, 2, 2, 3}) + ", modified array = " + arrayToString(new int[]{1, 1, 1, 2, 2, 3}, removeDuplicates(new int[]{1, 1, 1, 2, 2, 3})));
 
         System.out.println("For \"[0,0,1,1,1,1,2,3,3]\", new length = " + removeDuplicates(new int[]{0, 0, 1, 1, 1, 1, 2, 3, 3}) + ", modified array = " + arrayToString(new int[]{0, 0, 1, 1, 1, 1, 2, 3, 3}, removeDuplicates(new int[]{0, 0, 1, 1, 1, 1, 2, 3, 3})));
+
+        System.out.println("For \"[3,4]\", new length = " + removeDuplicates(new int[]{3,4}) + ", modified array = " + arrayToString(new int[]{3,4}, removeDuplicates(new int[]{3,4})));
+        
+        System.out.println("For \"[25]\", new length = " + removeDuplicates(new int[]{25}) + ", modified array = " + arrayToString(new int[]{25}, removeDuplicates(new int[]{25})));
+        
+        System.out.println("For \"[]\", new length = " + removeDuplicates(new int[]{}) + ", modified array = " + arrayToString(new int[]{}, removeDuplicates(new int[]{})));
     }
 
     public static int removeDuplicates(int[] nums) {
