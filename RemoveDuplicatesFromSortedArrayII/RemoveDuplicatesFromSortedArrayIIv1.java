6bd8a1c2a737db4143500291d06ef84c22e11e6a
CDAMXI <cdachongm@gmail.com>
2025-07-30 12:18:11 +0200

    Refactor removeDuplicates method to improve logic and enhance readability

--- RemoveDuplicatesFromSortedArrayII/RemoveDuplicatesFromSortedArrayIIv1.java
+++ RemoveDuplicatesFromSortedArrayII/RemoveDuplicatesFromSortedArrayIIv1.java
@@ -1,6 +1,7 @@
 public class RemoveDuplicatesFromSortedArrayIIv1{
     public static void main(String[] args){
         System.out.println("For: \"[1,1,1,2,2,3]\" the output is: " + removeDuplicates(new int[]{1,1,1,2,2,3}));
+        System.out.println("For: \"[0,0,1,1,1,1,2,3,3]\" the output is: " + removeDuplicates(new int[]{0,0,1,1,1,1,2,3,3}));
     }
 
     public static int removeDuplicates(int[] nums) {
